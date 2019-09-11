1fd2152c20991e06a46b1c1e6770818270861407
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 04:54:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,7 +3,7 @@ def third_challenge
    :montague => {
       :patriarch => {
         name: "Lord Montague",
-        age : "53"
+        age : '53'
         },
       :matriarch => {
         name: "Lady Montague",
