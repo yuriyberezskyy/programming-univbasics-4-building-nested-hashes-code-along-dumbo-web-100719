eeb55d517539368ab9f4b4518a8a463eecd3388b
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 05:07:55 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,20 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+  epic_tragedy = 
+  {
+    :montague => 
+    {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+    :capulet =>
+    {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+     }
+  }
 end
+
